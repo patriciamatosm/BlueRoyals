f61559fbad46f9a1f4abf02e0ea33c96da5430e4
Patri <pattymatos.m@gmail.com>
2024-07-08 19:49:47 +0200

    init

---
+++ subscription/subscription/src/main/java/es/uah/subscriptions/subscription/SubscriptionApplication.java
@@ -0,0 +1,13 @@
+package es.uah.subscriptions.subscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionApplication.class, args);
+	}
+
+}
